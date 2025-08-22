2c602f830008674b9ff11fb694290f66d99c8ae5
Eric Mbithi <mbithieri@gmail.com>
2025-08-22 22:44:40 +0300

    Object Initialization

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name, breed="Mutt"):
+        self.name = name
+        self.breed = breed
+
+
+dog1 = Dog("Buddy", "Golden Retriever")
+print(dog1.name)   # Buddy
+print(dog1.breed)  # Golden Retriever
+
+dog2 = Dog("Max")
+print(dog2.name)   # Max
+print(dog2.breed)  # Mutt
--- lib/person.py
+++ lib/person.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
 
 class Person:
+    def __init__(self, name="Guido"):
+        self.name = "Guido"
     pass
+
+guido = Person("Guido")
+print(guido.name)
